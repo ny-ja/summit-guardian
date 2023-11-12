a3b4174e50cfa6b2fa8f80e3ff13f7aa05f66f17
ny-ja <johnnytheklaw@gmail.com>
2023-11-12 10:23:22 +0800

    update map

--- src/components/Mapbox.vue
+++ src/components/Mapbox.vue
@@ -1,21 +1,52 @@
-<template>
-  <div ref="mapContainer" style="width: 100%; height: 400px;" />
-</template>
+<script>
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-<script setup>
-import { ref, onMounted } from 'vue';
-import mapboxgl from 'mapbox-gl';
+export default {
+  data() {
+    return {
+      map: null,
+      mapboxToken:
+        "pk.eyJ1IjoibnlqYSIsImEiOiJjbG1pZGRrZmIwbjI2M3JtZTNmYTFraXI4In0.NrAx6lDdsoXIfHg4K41e8w",
+    };
+  },
+  mounted() {
+    mapboxgl.accessToken = this.mapboxToken;
+    this.map = new mapboxgl.Map({
+      container: this.$refs.map,
+      style: "mapbox://styles/mapbox/outdoors-v11",
+      center: [0, 0], // default position
+      zoom: 14, // starting zoom
+    });
 
-const mapContainer = ref(null);
+    // Check if the browser supports geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        // Update the map's center and add a marker to the current position
+        this.updateLocation(
+          position.coords.longitude,
+          position.coords.latitude
+        );
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  },
+  methods: {
+    updateLocation(lng, lat) {
+      console.log(`Current Longitude: ${lng}, Current Latitude: ${lat}`); // This line will print the current longitude and latitude
 
-onMounted(() => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoibnlqYSIsImEiOiJjbG1pZGRrZmIwbjI2M3JtZTNmYTFraXI4In0.NrAx6lDdsoXIfHg4K41e8w';
+      new mapboxgl.Marker().setLngLat([lng, lat]).addTo(this.map);
 
-  const map = new mapboxgl.Map({
-    container: mapContainer.value,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [120.9842, 14.5995], // Longitude and latitude of Manila, Philippines
-    zoom: 10,
-  });
-});
+      this.map.flyTo({
+        center: [lng, lat],
+        zoom: 14, // adjust the zoom level as needed
+      });
+    },
+  },
+};
 </script>
+
+<template>
+  <div ref="map" style="width: 100%; height: 400px"></div>
+</template>
