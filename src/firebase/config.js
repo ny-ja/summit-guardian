3b3de3cac2817169b089488faff5c18d71dc1f89
ny-ja <johnnytheklaw@gmail.com>
2023-11-19 19:11:46 +0800

    update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiJhUBmb3WuXkMFxbwvxTdvMnfSCc8zPM",
-  authDomain: "summit-guardian-rtdb.firebaseapp.com",
-  databaseURL: "https://summit-guardian-rtdb-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "summit-guardian-rtdb",
-  storageBucket: "summit-guardian-rtdb.appspot.com",
-  messagingSenderId: "584551985261",
-  appId: "1:584551985261:web:de27edadb5e58c8e834f64"
+  apiKey: "AIzaSyBzQHnktl3MRsWiUdyGmzI6iBiPfi15COQ",
+  authDomain: "summit-guardian-db-fa6c5.firebaseapp.com",
+  databaseURL: "https://summit-guardian-db-fa6c5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "summit-guardian-db-fa6c5",
+  storageBucket: "summit-guardian-db-fa6c5.appspot.com",
+  messagingSenderId: "222627471801",
+  appId: "1:222627471801:web:3ce5f480a7bccf212b1668"
 };
 
 const app = initializeApp(firebaseConfig);
